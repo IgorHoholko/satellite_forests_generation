ee09a5121714cf176d67b50d49374e90e72d9d5e
Ihar <igor.xoxolko@gmail.com>
2021-03-07 18:31:04 +0300

    add base network classes

--- forest_generator/networks/__init__.py
+++ forest_generator/networks/__init__.py
@@ -5,3 +5,8 @@
  Date created : 27.02.2021
  Author:  Ihar Khakholka
 """
+
+from .NLayerDiscriminator import NLayerDiscriminator
+from .PixelDiscriminator import PixelDiscriminator
+from .resnet_generatorr import ResnetGenerator
+from .unet_generator import UnetGenerator
