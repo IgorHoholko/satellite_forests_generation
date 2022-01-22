5f331ff6df94956a085774eac9504b2106436929
Ihar <igor.xoxolko@gmail.com>
2022-01-22 19:22:22 +0300

    +add gan loss & gradient_penalty loss

---
+++ training/losses/__init__.py
@@ -0,0 +1,10 @@
+"""
+ File name   : __init__.py
+ Description : description
+
+ Date created : 22.01.2022
+ Author:  Ihar Khakholka
+"""
+
+from .gan_loss import GANLoss
+from .gradient_penalty import cal_gradient_penalty
