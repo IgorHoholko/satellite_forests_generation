b01612cf3a0aba97a37963c2f892b9c6fa20f335
Ihar <igor.xoxolko@gmail.com>
2022-01-22 15:18:23 +0300

    -remove unused training staff

--- training/callbacks/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .early_stopping import EarlyStopping
-from .model_checkpoint import ModelCheckpoint
-from .callbacks_factory import CallbacksFactory
-from .callbacks_container import CallbackContainer
--- training/run_experiment.py
+++
@@ -1,7 +0,0 @@
-"""
- File name   : run_experiment.py
- Description : description
-
- Date created : 27.02.2021
- Author:  Ihar Khakholka
-"""
